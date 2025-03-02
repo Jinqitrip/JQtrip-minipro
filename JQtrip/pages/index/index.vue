68c985f33cbc2dc99242b054c2ae6370c5f88ec2
fQwQf <supertjz123@foxmail.com>
2025-03-02 23:56:01 +0800

    优化首页帖子列表展示，调整数据结构和筛选逻辑

--- JQtrip/pages/index/index.vue
+++ JQtrip/pages/index/index.vue
@@ -38,213 +38,213 @@
 </template>
 
 <script>
-  import postitem from './postitem.vue';
-  export default {
-    components: {
-      postitem
-    },
-    data() {
-      return {
-        searchText: '',
-        postList: [{
-            "authoravatar": "/static/my.png",
-            "author": "邹某",
-            "guideavatar": "/static/my.png",
-            "guide": "哈哈哈哈哈哈哈",
-            "title": "游园武大",
-            "desc": "本科生 校园地陪 图书馆宿舍无法进入",
-            "locationName": "武汉大学",
-            "address": "湖北省武汉市武昌区八一路299号",
-            "time": "2小时"
-          },
-          {
-            "authoravatar": "/static/my.png",
-            "author": "邹某",
-            "guideavatar": "/static/my.png",
-            "guide": "嘻嘻嘻嘻",
-            "title": "游园HUST",
-            "desc": "本科生 校园地陪 图书馆宿舍无法进入",
-            "locationName": "华中科技大学",
-            "address": "湖北省武汉市武昌区八一路299号",
-            "time": "2小时"
-          },
-          {
-            "authoravatar": "/static/my.png",
-            "author": "邹某",
-            "guideavatar": "/static/my.png",
-            "guide": "徐云杰",
-            "title": "地大",
-            "desc": "本科生 校园地陪酒",
-            "locationName": "地质大学",
-            "address": "湖北省武汉市武昌区八一路299号",
-            "time": "2小时"
-          },
-        ],
-        isHovering: [],
-        areaList: ['全部', '武汉大学', '华中科技大学'],
-        areaIndex: 0
-      };
-    },
-    onLoad() {
-      this.isHovering = new Array(this.postList.length).fill(false);
-    },
-    methods: {
-      // getPostList() {
-      //   uni.request({
-      //     url: '',
-      //     method: '',
-      //     success: (res) => {
-      //       if (res.statusCode === 200) {
-      //         this.postList = res.data;
-      //         this.isHovering = new Array(this.postList.length).fill(false);
-      //       } else {
-      //         uni.showToast({
-      //           title: '获取数据失败',
-      //           icon: 'none'
-      //         });
-      //       }
-      //     },
-      //     fail: (err) => {
-      //       uni.showToast({
-      //         title: '网络请求失败，请检查网络',
-      //         icon: 'none'
-      //       });
-      //     }
-      //   });
-      // },
-      handleMouseEnter(index) {
-        this.isHovering[index] = true;
+import postitem from './postitem.vue';
+export default {
+  components: {
+    postitem
+  },
+  data() {
+    return {
+      searchText: '',
+      postList: [{
+        "authoravatar": "/static/my.png",
+        "author": "邹某",
+        "guideavatar": "/static/my.png",
+        "guide": "哈哈哈哈哈哈哈",
+        "title": "游园武大",
+        "desc": "本科生 校园地陪 图书馆宿舍无法进入",
+        "locationName": "武汉大学",
+        "address": "湖北省武汉市武昌区八一路299号",
+        "time": "2小时"
       },
-      handleMouseLeave(index) {
-        this.isHovering[index] = false;
+      {
+        "authoravatar": "/static/my.png",
+        "author": "邹某",
+        "guideavatar": "/static/my.png",
+        "guide": "嘻嘻嘻嘻",
+        "title": "游园HUST",
+        "desc": "本科生 校园地陪 图书馆宿舍无法进入",
+        "locationName": "华中科技大学",
+        "address": "湖北省武汉市武昌区八一路299号",
+        "time": "2小时"
       },
-      onAreaChange(e) {
-        this.areaIndex = e.detail.value;
-        // 过滤帖子
-        this.filterPosts();
+      {
+        "authoravatar": "/static/my.png",
+        "author": "邹某",
+        "guideavatar": "/static/my.png",
+        "guide": "徐云杰",
+        "title": "地大",
+        "desc": "本科生 校园地陪酒",
+        "locationName": "地质大学",
+        "address": "湖北省武汉市武昌区八一路299号",
+        "time": "2小时"
       },
-      filterPosts() {
-        if (this.areaIndex === 0) {
-          // 全部
-          this.filteredPostList = this.postList;
-        } else {
-          // 选择
-          const selectedArea = this.areaList[this.areaIndex];
-          this.filteredPostList = this.postList.filter(post => post.locationName === selectedArea);
-        }
-      }
+      ],
+      isHovering: [],
+      areaList: ['全部', '武汉大学', '华中科技大学'],
+      areaIndex: 0
+    };
+  },
+  onLoad() {
+    this.isHovering = new Array(this.postList.length).fill(false);
+  },
+  methods: {
+    // getPostList() {
+    //   uni.request({
+    //     url: '',
+    //     method: '',
+    //     success: (res) => {
+    //       if (res.statusCode === 200) {
+    //         this.postList = res.data;
+    //         this.isHovering = new Array(this.postList.length).fill(false);
+    //       } else {
+    //         uni.showToast({
+    //           title: '获取数据失败',
+    //           icon: 'none'
+    //         });
+    //       }
+    //     },
+    //     fail: (err) => {
+    //       uni.showToast({
+    //         title: '网络请求失败，请检查网络',
+    //         icon: 'none'
+    //       });
+    //     }
+    //   });
+    // },
+    handleMouseEnter(index) {
+      this.isHovering[index] = true;
     },
-    computed: {
-      filteredPostList() {
-        let filteredList = this.postList;
-        // 地区筛选
-        if (this.areaIndex !== 0) {
-          const selectedArea = this.areaList[this.areaIndex];
-          filteredList = filteredList.filter(post => post.locationName === selectedArea);
-        }
-        // 文本筛选
-        if (this.searchText) {
-          filteredList = filteredList.filter(post => {
-            return post.title.includes(this.searchText) || post.desc.includes(this.searchText);
-          });
-        }
-        return filteredList;
+    handleMouseLeave(index) {
+      this.isHovering[index] = false;
+    },
+    onAreaChange(e) {
+      this.areaIndex = e.detail.value;
+      // 过滤帖子
+      this.filterPosts();
+    },
+    filterPosts() {
+      if (this.areaIndex === 0) {
+        // 全部
+        this.filteredPostList = this.postList;
+      } else {
+        // 选择
+        const selectedArea = this.areaList[this.areaIndex];
+        this.filteredPostList = this.postList.filter(post => post.locationName === selectedArea);
       }
     }
-  };
+  },
+  computed: {
+    filteredPostList() {
+      let filteredList = this.postList;
+      // 地区筛选
+      if (this.areaIndex !== 0) {
+        const selectedArea = this.areaList[this.areaIndex];
+        filteredList = filteredList.filter(post => post.locationName === selectedArea);
+      }
+      // 文本筛选
+      if (this.searchText) {
+        filteredList = filteredList.filter(post => {
+          return post.title.includes(this.searchText) || post.desc.includes(this.searchText);
+        });
+      }
+      return filteredList;
+    }
+  }
+};
 </script>
 
 <style>
-  .container {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
-  .search-container {
-    position: fixed;
-    display: flex;
-    flex-direction: row-reverse;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 50px;
-    background-color: #ffffff;
-    display: flex;
-    align-items: center;
-    padding: 0 15px;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-    z-index: 100;
-  }
+.search-container {
+  position: fixed;
+  display: flex;
+  flex-direction: row-reverse;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  z-index: 100;
+}
 
-  .area-filter-box {
-    margin-right: 5px;
-  }
+.area-filter-box {
+  margin-right: 5px;
+}
 
-  .area-filter {
-    padding: 5px 10px;
-    background-color: #f5f5f5;
-    border-radius: 5px;
-  }
+.area-filter {
+  padding: 5px 10px;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+}
 
-  .logobox {
-    display: flex;
-    width: 10%;
-  }
+.logobox {
+  display: flex;
+  width: 10%;
+}
 
-  .search-input-box {
-    /* flex: 1; */
-    display: flex;
-    width: 50%;
-    flex-direction: row;
-    align-items: left;
-    text-align: left;
-    background-color: #f5f5f5;
-    border-radius: 20px;
-    padding: 10px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    margin-left: 5px;
-  }
+.search-input-box {
+  /* flex: 1; */
+  display: flex;
+  width: 50%;
+  flex-direction: row;
+  align-items: left;
+  text-align: left;
+  background-color: #f5f5f5;
+  border-radius: 20px;
+  padding: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 5px;
+}
 
-  .search-input {
-    flex: 1;
-    height: 35px;
-    border: none;
-    outline: none;
-    background-color: transparent;
-  }
+.search-input {
+  flex: 1;
+  height: 35px;
+  border: none;
+  outline: none;
+  background-color: transparent;
+}
 
-  .search-icon {
-    width: 20px;
-    height: 20px;
-    margin-left: 5px;
-  }
+.search-icon {
+  width: 20px;
+  height: 20px;
+  margin-left: 5px;
+}
 
-  .needpub {
-    margin-top: 65px;
-  }
+.needpub {
+  margin-top: 65px;
+}
 
-  .needbutton {
-    background-color: #acaf00;
-    border: none;
-    color: white;
-    width: 100%;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    cursor: pointer;
-    box-shadow: 0px 9px #999;
-  }
+.needbutton {
+  background-color: #acaf00;
+  border: none;
+  color: white;
+  width: 100%;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  box-shadow: 0px 9px #999;
+}
 
-  .needbutton:hover {
-    background-color: #af5700;
-  }
+.needbutton:hover {
+  background-color: #af5700;
+}
 
-  .needbutton:active {
-    background-color: #af5700;
-    box-shadow: 0px 5px #666;
-    transform: translateY(4px);
-  }
+.needbutton:active {
+  background-color: #af5700;
+  box-shadow: 0px 5px #666;
+  transform: translateY(4px);
+}
 </style>
--- JQtrip/pages/order/order_detail.vue
+++ JQtrip/pages/order/order_detail.vue
@@ -1,96 +1,266 @@
 <template>
-	<wd-card>
-	  <view class="content" @click.native="jump_to_order">
-	    <image :src="order.order_image" alt="joy"
-	      style="width: 100px; height: 100px; border-radius: 4px; margin-right: 12px" />
 
-	      <a style="color: black">AA专业导游</a>
+    <wd-card>
+        <view class="content" @click.native="jump_to_order">
+            <image :src="order.order_image" alt="joy"
+                style="width: 100px; height: 100px; border-radius: 4px; margin-right: 12px" />
 
-	  </view>
-	</wd-card>
-  <TnSteps v-model="currentStep" mode="dotIcon" style="width: 85%;margin: auto">
-    <TnStepsItem
-      v-for="(item, index) in stepsData"
-      :key="index"
-      :title="item.title"
-      :icon="item.icon"
-      :active-icon="item.activeIcon"
-    />
-  </TnSteps>
+            <view class="info-container">
+                <text class="title">{{ order.title }}</text>
+
+                <view class="detail-row">
+                    <view class="detail-item">
+                        <tn-icon name="location" size="16" color="#666"></tn-icon>
+                        <text class="detail-text">{{ order.location }}</text>
+                    </view>
+                    <view class="detail-item">
+                        <tn-icon name="money" size="16" color="#666"></tn-icon>
+                        <text class="detail-text">¥{{ order.price }}</text>
+                    </view>
+                </view>
+
+                <view class="detail-row">
+                    <view class="detail-item">
+                        <tn-icon name="time" size="16" color="#666"></tn-icon>
+                        <text class="detail-text">{{ order.time }}</text>
+                    </view>
+                </view>
+            </view>
+        </view>
+    </wd-card>
+
+    <TnSteps v-model="currentStep" mode="dotIcon" style="width: 85%;margin: auto">
+        <TnStepsItem v-for="(item, index) in stepsData" :key="index" :title="item.title" :icon="item.icon"
+            :active-icon="item.activeIcon" />
+    </TnSteps>
+
+    <view class="content-box">
+
+
+        <view class="info-grid">
+            <view class="info-item">
+                <text class="info-label">服务时长</text>
+                <text class="info-value">3小时</text>
+            </view>
+            <view class="info-item">
+                <text class="info-label">服务人数</text>
+                <text class="info-value">1-5人</text>
+            </view>
+            <view class="info-item">
+                <text class="info-label">联系方式</text>
+                <text class="info-value">138****5678</text>
+            </view>
+        </view>
+
+        <text class="detail-content">
+            {{ order.detail || '暂无详细信息' }}
+        </text>
+
+    </view>
+
+    <wd-button block plain v-if="order.step == 1" @click="jump_to_select">选择导游</wd-button>
+
 </template>
 
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent } from 'vue'
 import TnSteps from '@/uni_modules/tuniaoui-vue3/components/steps/src/steps.vue'
 import TnStepsItem from '@/uni_modules/tuniaoui-vue3/components/steps/src/steps-item.vue'
-import { ref } from 'vue'
 
+export default defineComponent({
+    components: {
+        TnSteps,
+        TnStepsItem
+    },
+    data() {
+        return {
+            order: {
+                title: "AA专业导游",
+                order_image: "/static/logo.png",
+                location: "南二门",
+                price: "200",
+                time: "2025/3/1 14:00",
+                step: 1,
+                detail: "专业导游服务包含：\n· 景点深度讲解\n· 游览路线规划\n· 文化历史介绍\n· 全程陪同服务\n· 基础摄影指导"
+            },
+            stepsData: [
+                {
+                    title: '已下单',
+                    icon: 'success-circle',
+                    activeIcon: 'success-circle-fill',
+                },
+                {
+                    title: '待选择',
+                    icon: 'success-circle',
+                    activeIcon: 'success-circle-fill',
+                },
+                {
+                    title: '待开始',
+                    icon: 'success-circle',
+                    activeIcon: 'success-circle-fill',
+                },
+                {
+                    title: '已完成',
+                    icon: 'success-circle',
+                    activeIcon: 'success-circle-fill',
+                },
+            ]
+        }
+    },
+    computed: {
+        currentStep(): number {
+            return this.order.step
+        }
+    },
+    onLoad(option: any) {
+        try {
+            const data = JSON.parse(option.index)
+            this.order = Object.assign({}, this.order, data)
+        } catch (e) {
+            console.error('数据解析失败:', e)
+        }
+    },
+    methods: {
+        jump_to_select(order) {
+            var mynavData = JSON.stringify(order);
+            uni.navigateTo({
+                url: "/pages/order/order-select?index=" + mynavData
+            });
+            console.log(321);
+        }
 
-const order = {
-        "order_image": "/static/logo.png",
-        "location": "南二门",
-        "price": "200",
-        "time": "2025/3/1 14:00",
-        "step": 1
-      }
-	  
-const currentStep = order.step
-
-// 步进条数据
-const stepsData = [
-  {
-    title: '已下单',
-    icon: 'success-circle',
-    activeIcon: 'success-circle-fill',
-  },
-  {
-    title: '待选择',
-    icon: 'success-circle',
-    activeIcon: 'success-circle-fill',
-  },
-  {
-    title: '待开始',
-    icon: 'success-circle',
-    activeIcon: 'success-circle-fill',
-  },
-  {
-    title: '已完成',
-    icon: 'success-circle',
-    activeIcon: 'success-circle-fill',
-  },
-]
+    }
+})
 </script>
 
 <style>
-	.content,
-	.title {
-	  display: flex;
-	  flex-direction: row;
-	  justify-content: flex-start;
-	  align-items: center;
-	}
-	
-	.content {
-	  justify-content: flex-start;
-	}
-	
-	.title {
-	  justify-content: space-between;
-	}
-	
-	.title-tip {
-	  color: rgba(0, 0, 0, 0.25);
-	  font-size: 12px;
-	}
-	
-	.custom-main {
-	  color: rgba(0, 0, 0, 0.85);
-	  font-size: 16px;
-	}
-	
-	.custom-sub {
-	  color: rgba(0, 0, 0, 0.25);
-	  font-size: 12px;
-	}
-  
+.detail-section {
+    padding: 16px;
+}
+
+.section-title {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #eee;
+}
+
+.title-text {
+    font-size: 16px;
+    font-weight: 600;
+    color: #333;
+    margin-left: 8px;
+}
+
+.content-box {
+    background: #f8f9fa;
+    border-radius: 8px;
+    padding: 12px;
+}
+
+.detail-content {
+    font-size: 14px;
+    color: #666;
+    line-height: 1.6;
+    white-space: pre-wrap;
+}
+
+.info-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 12px;
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid #eee;
+}
+
+.info-item {
+    background: white;
+    border-radius: 6px;
+    padding: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+}
+
+.info-label {
+    font-size: 12px;
+    color: #999;
+    display: block;
+    margin-bottom: 4px;
+}
+
+.info-value {
+    font-size: 14px;
+    color: #333;
+    font-weight: 500;
+}
+
+.content {
+    display: flex;
+    padding: 16px;
+    align-items: flex-start;
+}
+
+.info-container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+.title {
+    font-size: 16px;
+    font-weight: 500;
+    color: #333;
+    line-height: 1.4;
+}
+
+.detail-row {
+    display: flex;
+    gap: 20px;
+}
+
+.detail-item {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+}
+
+.detail-text {
+    font-size: 14px;
+    color: #666;
+    line-height: 1.4;
+}
+
+.content,
+.title {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.content {
+    justify-content: flex-start;
+}
+
+.title {
+    justify-content: space-between;
+}
+
+.title-tip {
+    color: rgba(0, 0, 0, 0.25);
+    font-size: 12px;
+}
+
+.custom-main {
+    color: rgba(0, 0, 0, 0.85);
+    font-size: 16px;
+}
+
+.custom-sub {
+    color: rgba(0, 0, 0, 0.25);
+    font-size: 12px;
+}
 </style>
