33e8b34186972160b129384a23f50c1296a4c56a
fQwQf <supertjz123@foxmail.com>
2025-03-12 14:40:00 +0800

    feat(user): 微信登录功能增强
    - 在全局数据中添加 avatarUrl 和 nickName 字段
    - 完善微信登录逻辑，获取并存储用户昵称和头像
    - 优化代码结构，提高可读性和可维护性

--- JQtrip/pages/my/login_wx.vue
+++ JQtrip/pages/my/login_wx.vue
@@ -1,97 +1,94 @@
 <template>
-  <view>
-    <button @click="login">微信登录</button>
-	<TnIcon custom-class="home" :custom-style="style" />
-  </view>
+	<view>
+		<button @click="login">微信登录</button>
+		<TnIcon custom-class="home" :custom-style="style" />
+	</view>
 </template>
 
 <script>
 import { baseUrl } from '@/config';
 export default {
-  data() {
-    return {
-      userInfo: null,
-      hasUserInfo: false
-    }
-  },
-  methods: {
-    // 登录并获取用户信息
-    login() {
-		uni.getUserInfo({
-		  provider: 'weixin',
-		  success: function (infoRes) {
-			console.log(infoRes.userInfo)
-		
-			this.$userData.userInfo = infoRes.userInfo;
-			
-			uni.setStorageSync('userInfo', this.$userData.userInfo);
-		
-			
-			
-		  },
-		  fail: () => {
-		  	uni.showToast({
-		  	  title: '用户信息获取失败',
-		  	  icon: 'none'
-		  	});
-		  }
-		});
+	data() {
+		return {
+			userInfo: null,
+			hasUserInfo: false
+		}
+	},
+	methods: {
+		// 登录并获取用户信息
+		login() {
+			uni.getUserInfo({
+				provider: 'weixin',
+				success: (infoRes) => {
+					console.log(infoRes.userInfo)
 
-      uni.login({
-        provider: 'weixin',
-        success: (loginRes) => {
-          // 登录成功，获取用户code
-          const { code } = loginRes;
-		  console.log(code);
-          // 发送code到后台换取openId, sessionKey, unionId
-            uni.request({
-            url: baseUrl+"/v1/auth/wxLogin", // 你的登录API地址
-            method: 'POST',
-            data: {
-              "code":code
-            },
-            success: (res) => {
-              if (res.data && res.data.success) {
+					this.$userData.nickName = infoRes.userInfo.nickName;
+					this.$userData.avatarUrl = infoRes.userInfo.avatarUrl;
+					uni.setStorageSync('nickName', this.$userData.nickName);
+					uni.setStorageSync('avatarUrl', this.$userData.avatarUrl);
+
+				},
+				fail: () => {
+					uni.showToast({
+						title: '用户信息获取失败',
+						icon: 'none'
+					});
+				}
+			});
+
+			uni.login({
+				provider: 'weixin',
+				success: (loginRes) => {
+					// 登录成功，获取用户code
+					const { code } = loginRes;
+					console.log(code);
+					// 发送code到后台换取openId, sessionKey, unionId
+					uni.request({
+						url: baseUrl + "/v1/auth/wxLogin", // 你的登录API地址
+						method: 'POST',
+						data: {
+							"code": code
+						},
+						success: (res) => {
+							if (res.data && res.data.success) {
+
+								//  载入全局变量
+								this.$userData.openId = res.data.openId;
+								this.$userData.sessionKey = res.data.sessionKey;
+
+								// 将用户信息和session存储到本地
+								uni.setStorageSync('openId', this.$userData.openId);
+								uni.setStorageSync('sessionKey', this.$userData.sessionKey);
+
+								console.log(res);
 
 
-					//  载入全局变量
-					this.$userData.openId = res.data.openId;
-					this.$userData.sessionKey = res.data.sessionKey;
-					
-					// 将用户信息和session存储到本地
-					uni.setStorageSync('openId', this.$userData.openId);
-					uni.setStorageSync('sessionKey', this.$userData.sessionKey);
-					
-					console.log(res);
-					
-				
-              } else {
-                uni.showToast({
-                  title: '登录失败',
-                  icon: 'none'
-                });
-              }
-            },
-            fail: () => {
-              uni.showToast({
-                title: '请求失败',
-                icon: 'none'
-              });
-            }
-          }); 
-        },
-        fail: (err) => {
-          console.log('uni.login 接口调用失败，将无法正常使用开放接口等服务', err);
-          uni.showToast({
-            title: '登录失败',
-            icon: 'none'
-          });
-        }
-      });
-    }
-  }
+							} else {
+								uni.showToast({
+									title: '登录失败',
+									icon: 'none'
+								});
+							}
+						},
+						fail: () => {
+							uni.showToast({
+								title: '请求失败',
+								icon: 'none'
+							});
+						}
+					});
+				},
+				fail: (err) => {
+					console.log('uni.login 接口调用失败，将无法正常使用开放接口等服务', err);
+					uni.showToast({
+						title: '登录失败',
+						icon: 'none'
+					});
+				}
+			});
+		}
+	}
 }
 </script>
 
-<style>
-</style>
+<style></style>
