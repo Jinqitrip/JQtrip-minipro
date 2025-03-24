c97d0afdeb3ad93adbe16cd17306f207d4f78ad9
fQwQf <supertjz123@foxmail.com>
2025-03-24 11:20:46 +0800

    refactor(pages/index): 优化首页布局和样式
    - 调整了顶部导航栏的样式
    - 优化了添加需求卡片的布局
    - 统一了登录提示模态框的样式
    - 调整了帖子列表项的样式
    - 优化了代码格式和结构

--- JQtrip/pages/index/index.vue
+++ JQtrip/pages/index/index.vue
@@ -1,6 +1,8 @@
 <template>
   <view class="container">
+
     <!-- 顶部导航栏 -->
+    <!--
     <view class="search-container">
       <view class="area-filter-box">
         <picker @change="onAreaChange" :value="areaIndex" :range="areaList">
@@ -22,29 +24,30 @@
         <image class="search-icon" src="/static/search_icon.png" mode="aspectFit"></image>
       </view>
     </view>
+  -->
 
-  <!-- 添加需求模块 -->
-  <view class="demand-card" @click="handleAddDemand">
-    <view class="demand-content">
-      <image class="add-icon" src="/static/plus-circle-filled.svg"></image>
-      <text class="demand-title">创建个性游览需求</text>
-      <text class="demand-subtitle">点击定制专属旅程体验</text>
+    <!-- 添加需求模块 -->
+    <view class="demand-card" @click="handleAddDemand">
+      <view class="demand-content">
+        <image class="add-icon" src="/static/plus-circle-filled.svg"></image>
+        <text class="demand-title">创建个性游览需求</text>
+        <text class="demand-subtitle">点击定制专属旅程体验</text>
+      </view>
+      <image class="decorative-pattern" src="/static/wave-pattern.png"></image>
     </view>
-    <image class="decorative-pattern" src="/static/wave-pattern.png"></image>
-  </view>
 
-  <!-- 登录提示模态框 -->
-  <view v-if="showLoginModal" class="login-modal-mask">
-    <view class="login-modal">
-      <image class="modal-icon" src="/static/login-required.png"></image>
-      <text class="modal-title">需要登录</text>
-      <text class="modal-desc">登录后可以创建和查看个性化需求</text>
-      <view class="button-group">
-        <view class="modal-btn cancel-btn" @click="showLoginModal = false">稍后再说</view>
-        <view class="modal-btn confirm-btn" @click="gotoLogin">立即登录</view>
+    <!-- 登录提示模态框 -->
+    <view v-if="showLoginModal" class="login-modal-mask">
+      <view class="login-modal">
+        <image class="modal-icon" src="/static/login-required.png"></image>
+        <text class="modal-title">需要登录</text>
+        <text class="modal-desc">登录后可以创建和查看个性化需求</text>
+        <view class="button-group">
+          <view class="modal-btn cancel-btn" @click="showLoginModal = false">稍后再说</view>
+          <view class="modal-btn confirm-btn" @click="gotoLogin">立即登录</view>
+        </view>
       </view>
     </view>
-  </view>
 
     <!-- 帖子列表区域 -->
     <view class="post-list-container">
@@ -63,84 +66,84 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        searchText: '',
-        postList: [{
-            id: 4,
-            authoravatar: "/static/my.png",
-            author: "Harry",
-            guideavatar: "/static/my.png",
-            guide: "Andy",
-            title: "游园武大",
-            briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
-            desc: "111111111111111111111111113zhehshdifhsfjdjkdjksjksjfisadjsaifjsafjsdfjfhsdfhfoiaw",
-            locationName: "武汉大学",
-            address: "湖北省武汉市武昌区八一路299号",
-            time: "2小时",
-            images: ["/static/my.png", "/static/order.png", "/static/order.png"]
-          },
-          {
-            id: 3,
-            authoravatar: "/static/my.png",
-            author: "Harry",
-            guideavatar: "/static/my.png",
-            guide: "Andy",
-            title: "游园武大",
-            briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
-            desc: "111111111111111111111111113zhehshdifhsfjdjkdjksjksjfisadjsaifjsafjsdfjfhsdfhfoiaw",
-            locationName: "武汉大学",
-            address: "湖北省武汉市武昌区八一路299号",
-            time: "2小时",
-            images: ["/static/my.png", "/static/order.png", "/static/order.png"]
-          }, {
-            id: 1,
-            authoravatar: "/static/my.png",
-            author: "Harry",
-            guideavatar: "/static/my.png",
-            guide: "Andy",
-            title: "游园武大",
-            briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
-            desc: "111111111111111111111111113zhehshdifhsfjdjkdjksjksjfisadjsaifjsafjsdfjfhsdfhfoiaw",
-            locationName: "武汉大学",
-            address: "湖北省武汉市武昌区八一路299号",
-            time: "2小时",
-            images: ["/static/my.png", "/static/order.png", "/static/order.png"]
-          },
-          {
-            id: 2,
-            authoravatar: "/static/my.png",
-            author: "Harry",
-            guideavatar: "/static/my.png",
-            guide: "Cindy",
-            title: "游园武大",
-            briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
-            desc: "11111111111111111111111111111111",
-            locationName: "武汉大学",
-            address: "湖北省武汉市武昌区八一路299号",
-            time: "2小时",
-            images: ["/static/home.png"]
-          },
-        ],
-        areaIndex: '0',
-        areaList: ['全部', '武汉大学', '华中科技大学', '中国地质大学'],
-        currentPost: null,
-        showDetail: false,
-		showLoginModal: false
-      };
+export default {
+  data() {
+    return {
+      searchText: '',
+      postList: [{
+        id: 4,
+        authoravatar: "/static/my.png",
+        author: "Harry",
+        guideavatar: "/static/my.png",
+        guide: "Andy",
+        title: "游园武大",
+        briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
+        desc: "111111111111111111111111113zhehshdifhsfjdjkdjksjksjfisadjsaifjsafjsdfjfhsdfhfoiaw",
+        locationName: "武汉大学",
+        address: "湖北省武汉市武昌区八一路299号",
+        time: "2小时",
+        images: ["/static/my.png", "/static/order.png", "/static/order.png"]
+      },
+      {
+        id: 3,
+        authoravatar: "/static/my.png",
+        author: "Harry",
+        guideavatar: "/static/my.png",
+        guide: "Andy",
+        title: "游园武大",
+        briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
+        desc: "111111111111111111111111113zhehshdifhsfjdjkdjksjksjfisadjsaifjsafjsdfjfhsdfhfoiaw",
+        locationName: "武汉大学",
+        address: "湖北省武汉市武昌区八一路299号",
+        time: "2小时",
+        images: ["/static/my.png", "/static/order.png", "/static/order.png"]
+      }, {
+        id: 1,
+        authoravatar: "/static/my.png",
+        author: "Harry",
+        guideavatar: "/static/my.png",
+        guide: "Andy",
+        title: "游园武大",
+        briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
+        desc: "111111111111111111111111113zhehshdifhsfjdjkdjksjksjfisadjsaifjsafjsdfjfhsdfhfoiaw",
+        locationName: "武汉大学",
+        address: "湖北省武汉市武昌区八一路299号",
+        time: "2小时",
+        images: ["/static/my.png", "/static/order.png", "/static/order.png"]
+      },
+      {
+        id: 2,
+        authoravatar: "/static/my.png",
+        author: "Harry",
+        guideavatar: "/static/my.png",
+        guide: "Cindy",
+        title: "游园武大",
+        briefdesc: "本科生 校园地陪 图书馆宿舍无法进入",
+        desc: "11111111111111111111111111111111",
+        locationName: "武汉大学",
+        address: "湖北省武汉市武昌区八一路299号",
+        time: "2小时",
+        images: ["/static/home.png"]
+      },
+      ],
+      areaIndex: '0',
+      areaList: ['全部', '武汉大学', '华中科技大学', '中国地质大学'],
+      currentPost: null,
+      showDetail: false,
+      showLoginModal: false
+    };
+  },
+  methods: {
+    onAreaChange(e) {
+      this.areaIndex = e.detail.value;
     },
-    methods: {
-      onAreaChange(e) {
-        this.areaIndex = e.detail.value;
-      },
-      gotodemand() {
-        uni.navigateTo({
-          url: '/pages/index/demandfill'
-        });
-      },
+    gotodemand() {
+      uni.navigateTo({
+        url: '/pages/index/demandfill'
+      });
+    },
     handleAddDemand() {
-      if (this.$userData.openId=="") {
+      if (this.$userData.openId == "") {
         this.showLoginModal = true;
         return;
       }
@@ -151,171 +154,171 @@
     gotoLogin() {
       uni.navigateTo({ url: '/pages/my/login_wx' });
       this.showLoginModal = false;
+    },
+
+    goToPostDetail(post) {
+      uni.navigateTo({
+        url: `/pages/index/postdetail?postData=${JSON.stringify(post)}`
+      });
     }
-      },
-      goToPostDetail(post) {
-        uni.navigateTo({
-          url: `/pages/index/postdetail?postData=${JSON.stringify(post)}`
+  },
+  computed: {
+    filteredPostList() {
+      let filteredList = this.postList;
+      // 地区筛选
+      if (this.areaIndex !== '0') {
+        const selectedArea = this.areaList[this.areaIndex];
+        filteredList = filteredList.filter(post => post.locationName === selectedArea);
+      }
+      // 文本筛选
+      if (this.searchText) {
+        filteredList = filteredList.filter(post => {
+          return post.title.includes(this.searchText) || post.desc.includes(this.searchText);
         });
       }
-    },
-    computed: {
-      filteredPostList() {
-        let filteredList = this.postList;
-        // 地区筛选
-        if (this.areaIndex !== '0') {
-          const selectedArea = this.areaList[this.areaIndex];
-          filteredList = filteredList.filter(post => post.locationName === selectedArea);
-        }
-        // 文本筛选
-        if (this.searchText) {
-          filteredList = filteredList.filter(post => {
-            return post.title.includes(this.searchText) || post.desc.includes(this.searchText);
-          });
-        }
-        return filteredList;
-      }
+      return filteredList;
     }
-  };
+  }
+}
 </script>
 
 <style>
-  .container {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
-  .search-container {
-    position: fixed;
-    display: flex;
-    flex-direction: row-reverse;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 50px;
-    background-color: #ffffff;
-    align-items: center;
-    padding: 0 15px;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-    z-index: 100;
-  }
+.search-container {
+  position: fixed;
+  display: flex;
+  flex-direction: row-reverse;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  background-color: #ffffff;
+  align-items: center;
+  padding: 0 15px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  z-index: 100;
+}
 
-  .area-filter-box {
-    margin-right: 5px;
-  }
+.area-filter-box {
+  margin-right: 5px;
+}
 
-  .area-filter {
-    padding: 5px 10px;
-    background-color: #f5f5f5;
-    border-radius: 5px;
-  }
+.area-filter {
+  padding: 5px 10px;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+}
 
-  .logobox {
-    display: flex;
-    width: 10%;
-  }
+.logobox {
+  display: flex;
+  width: 10%;
+}
 
-  .search-input-box {
-    display: flex;
-    width: 50%;
-    flex-direction: row;
-    align-items: left;
-    text-align: left;
-    background-color: #f5f5f5;
-    border-radius: 20px;
-    padding: 10px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    margin-left: 5px;
-  }
+.search-input-box {
+  display: flex;
+  width: 50%;
+  flex-direction: row;
+  align-items: left;
+  text-align: left;
+  background-color: #f5f5f5;
+  border-radius: 20px;
+  padding: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 5px;
+}
 
-  .search-input {
-    flex: 1;
-    height: 35px;
-    border: none;
-    outline: none;
-    background-color: transparent;
-  }
+.search-input {
+  flex: 1;
+  height: 35px;
+  border: none;
+  outline: none;
+  background-color: transparent;
+}
 
-  .search-icon {
-    width: 20px;
-    height: 20px;
-    margin-left: 5px;
-  }
+.search-icon {
+  width: 20px;
+  height: 20px;
+  margin-left: 5px;
+}
 
-  .needframe {
-    height: 240px;
-    margin-top: 70px;
-    margin-left: 5px;
-    margin-right: 5px;
-    margin-bottom: 20px;
-    border-radius: 5px;
-    box-shadow: 1px 2px 10px 0px rgba(0, 0, 0, 0.3);
-    background: #3A92AF;
-    background: linear-gradient(to top right, #00ffff 0%, #ffffff 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#3A92AF', endColorstr='#5CA05A', GradientType=1);
-    color: #fff;
-    font-family: 'Open Sans', Helvetica, sans-serif;
-  }
+.needframe {
+  height: 240px;
+  margin-top: 70px;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  box-shadow: 1px 2px 10px 0px rgba(0, 0, 0, 0.3);
+  background: #3A92AF;
+  background: linear-gradient(to top right, #00ffff 0%, #ffffff 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#3A92AF', endColorstr='#5CA05A', GradientType=1);
+  color: #fff;
+  font-family: 'Open Sans', Helvetica, sans-serif;
+}
 
-  .center {
-    height: 200px;
-    margin-top: 20px;
-    margin-left: 20px;
-    margin-right: 20px;
-    background: #fff;
-    box-shadow: 8px 10px 15px 0 rgba(0, 0, 0, 0.2);
-    border-radius: 3px;
-  }
+.center {
+  height: 200px;
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  background: #fff;
+  box-shadow: 8px 10px 15px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+}
 
-  .title {
-    font-size: 16px;
-    color: #676767;
-    line-height: 50px;
-    height: 50px;
-    border-bottom: 1px solid #D8D8D8;
-    text-align: center;
-  }
+.title {
+  font-size: 16px;
+  color: #676767;
+  line-height: 50px;
+  height: 50px;
+  border-bottom: 1px solid #D8D8D8;
+  text-align: center;
+}
 
-  .addzone {
-    box-sizing: border-box;
-    width: 100px;
-    height: 100px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 20px;
-    border: 1px dashed #A4A4A4;
-    border-radius: 3px;
-    text-align: center;
-  }
+.addzone {
+  box-sizing: border-box;
+  width: 100px;
+  height: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  border: 1px dashed #A4A4A4;
+  border-radius: 3px;
+  text-align: center;
+}
 
-  .plusicon {
-    width: 98px;
-    height: 98px;
-  }
+.plusicon {
+  width: 98px;
+  height: 98px;
+}
 
-  .post-list-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding: 10px;
-  }
+.post-list-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 10px;
+}
 
-  .post-item-small {
-    width: calc(50% - 5px);
-    margin-bottom: 10px;
-    border-radius: 8px;
-    overflow: hidden;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
+.post-item-small {
+  width: calc(50% - 5px);
+  margin-bottom: 10px;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
-  .post-abb-desc {
-    display: flex;
-    flex-direction: column;
-    margin-top: 10px;
-  }
-  
+.post-abb-desc {
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+}
+
 /* 添加需求卡片样式 */
 .demand-card {
   position: relative;
@@ -346,7 +349,7 @@
   width: 60rpx;
   height: 60rpx;
   margin-bottom: 20rpx;
-  filter: drop-shadow(0 4rpx 8rpx rgba(0,0,0,0.1));
+  filter: drop-shadow(0 4rpx 8rpx rgba(0, 0, 0, 0.1));
 }
 
 .demand-title {
@@ -358,7 +361,7 @@
 
 .demand-subtitle {
   font-size: 26rpx;
-  color: rgba(255,255,255,0.9);
+  color: rgba(255, 255, 255, 0.9);
 }
 
 .decorative-pattern {
@@ -377,7 +380,7 @@
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -386,7 +389,7 @@
 
 .login-modal {
   width: 600rpx;
-  
+
   background: #FFFFFF;
   border-radius: 32rpx;
   padding: 50rpx;
@@ -395,8 +398,15 @@
 }
 
 @keyframes modalShow {
-  from { transform: scale(0.8); opacity: 0; }
-  to { transform: scale(1); opacity: 1; }
+  from {
+    transform: scale(0.8);
+    opacity: 0;
+  }
+
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
 }
 
 .modal-icon {
@@ -455,24 +465,23 @@
 .confirm-btn:active {
   background: #4A97DB;
 }
-  }
 
-  .post-small-img {
-    width: 100%;
-    height: auto;
-    aspect-ratio: 1 / 1;
-    object-fit: cover;
-  }
+.post-small-img {
+  width: 100%;
+  height: auto;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+}
 
-  .post-small-title {
-    font-size: 14px;
-    font-weight: 600;
-    padding: 5px;
-  }
+.post-small-title {
+  font-size: 14px;
+  font-weight: 600;
+  padding: 5px;
+}
 
-  .post-small-author {
-    font-size: 12px;
-    color: #666;
-    padding: 0 5px 5px 5px;
-  }
+.post-small-author {
+  font-size: 12px;
+  color: #666;
+  padding: 0 5px 5px 5px;
+}
 </style>
